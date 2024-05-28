3427216550775a9d543e4d9c6d75be2fad99d430
lee <krleepi@gamil.com>
2024-05-28 22:54:53 +1000

    worked on navbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import LandingPage from './pages/LandingPage';
 import RecommendedMoviesPage from './pages/RecommendedMoviesPage';
 import RateMoviePage from './pages/RateMoviePage';
 import AllMoviesPage from './pages/AllMoviesPage';
+import Navbar from './pages/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/recommended" element={<RecommendedMoviesPage />} />
-      <Route path="/rate" element={<RateMoviePage />} />
-      <Route path="/allmovies" element={<AllMoviesPage />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/recommended" element={<RecommendedMoviesPage />} />
+        <Route path="/rate" element={<RateMoviePage />} />
+        <Route path="/allmovies" element={<AllMoviesPage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../Styles/Navbar.css";
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <div className='middle'>
+                <Link to="/"> Home </Link>
+                <Link to="/recommended"> Recommended </Link>
+                <Link to="/rate"> Rate </Link>
+                <Link to="/allmovies"> Movies </Link>
+            </div>
+        </div>
+  </div>
+  )
+}
+
+export default Navbar
