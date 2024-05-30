fa674099196e06821ba8f3b7a8a8e1b88bb3c941
lee <krleepi@gamil.com>
2024-05-30 22:06:45 +1000

    .

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className='landing-page'>
+      <div className='question'>
+        Which genre do you like?
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
