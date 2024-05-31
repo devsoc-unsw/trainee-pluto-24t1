13a95f4a9fca63abdd7f08e3c698d523593c3483
lee <krleepi@gamil.com>
2024-05-31 11:49:47 +1000

    worked on the nav bar a bit

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../Styles/Navbar.css";
 
-
 const Navbar = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleDropdownToggle = (e) => {
+    e.preventDefault();  // Prevent the default link behavior
+    setDropdownVisible(prev => !prev);
+  };
+
   return (
     <div>
-        <div className='navbar'>
-            <div className='left'>
-            Team Pluto 🌑
-            </div>
-            <div className='middle'>
-                <Link to="/home"> Home </Link>
-                <Link to="/recommended/action"> Recommended </Link>
-                <Link to="/rate"> Rate </Link>
-                {/* <Link to="/allmovies"> Movies </Link> */}
-            </div>
-            <div className='right'>
-            Team Pluto 🌑
-            </div>
+      <div className='navbar'>
+        <div className='left'>
+          Pluto 🌑
         </div>
-  </div>
-  )
+        <div className='middle'>
+          <Link to="/home"> Home </Link>
+          <div className="dropdown">
+            <a href="#" onClick={handleDropdownToggle}> Recommended </a>
+            {dropdownVisible && (
+              <div className="dropdown-content">
+                <Link to="/recommended/thriller">Thriller</Link>
+                <Link to="/recommended/action">Action</Link>
+                <Link to="/recommended/adventure">Adventure</Link>
+                <Link to="/recommended/comedy">Comedy</Link>
+                <Link to="/recommended/drama">Drama</Link>
+                <Link to="/recommended/horror">Horror</Link>
+                <Link to="/recommended/romance">Romance</Link>
+                <Link to="/recommended/animation">Animation</Link>
+                <Link to="/recommended/anime">Anime</Link>
+                <Link to="/recommended/sciencefiction">SciFi</Link>
+              </div>
+            )}
+          </div>
+          <Link to="/rate"> Rate </Link>
+          <Link to="/allmovies"> Upcoming </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
