b1526ae7b834cf6283ed3f518012a844aa56d3ab
Winn Z <winnwyz@gmail.com>
2025-03-20 14:47:35 +1100

    Use hashrouter for deployment

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { NameProvider } from './NameContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <NameProvider>
         <App />
       </NameProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
