3e9240d49de04fe8b2ae9c7f9d640330744eed71
Winn Z <winnwyz@gmail.com>
2025-03-20 13:43:01 +1100

    Implement navbar close on click on elements

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import "../Styles/Navbar.css";
 
 const Navbar = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleDropdownToggle = (e) => {
     e.preventDefault();  // Prevent the default link behavior
     setDropdownVisible(prev => !prev);
+  };
+
+  const handleGenreClick = () => {
+    setDropdownVisible(false);
   };
 
   return (
@@ -18,20 +36,20 @@ const Navbar = () => {
         </div>
         <div className='middle'>
           <Link to="/home"> Home </Link>
-          <div className="dropdown">
+          <div className="dropdown" ref={dropdownRef}>
             <a href="#" onClick={handleDropdownToggle}> Recommended </a>
             {dropdownVisible && (
               <div className="dropdown-content">
-                <Link to="/recommended/thriller">Thriller</Link>
-                <Link to="/recommended/action">Action</Link>
-                <Link to="/recommended/adventure">Adventure</Link>
-                <Link to="/recommended/comedy">Comedy</Link>
-                <Link to="/recommended/drama">Drama</Link>
-                <Link to="/recommended/horror">Horror</Link>
-                <Link to="/recommended/romance">Romance</Link>
-                <Link to="/recommended/animation">Animation</Link>
-                <Link to="/recommended/anime">Anime</Link>
-                <Link to="/recommended/sciencefiction">SciFi</Link>
+                <Link to="/recommended/thriller" onClick={handleGenreClick}>Thriller</Link>
+                <Link to="/recommended/action" onClick={handleGenreClick}>Action</Link>
+                <Link to="/recommended/adventure" onClick={handleGenreClick}>Adventure</Link>
+                <Link to="/recommended/comedy" onClick={handleGenreClick}>Comedy</Link>
+                <Link to="/recommended/drama" onClick={handleGenreClick}>Drama</Link>
+                <Link to="/recommended/horror" onClick={handleGenreClick}>Horror</Link>
+                <Link to="/recommended/romance" onClick={handleGenreClick}>Romance</Link>
+                <Link to="/recommended/animation" onClick={handleGenreClick}>Animation</Link>
+                <Link to="/recommended/anime" onClick={handleGenreClick}>Anime</Link>
+                <Link to="/recommended/sciencefiction" onClick={handleGenreClick}>SciFi</Link>
               </div>
             )}
           </div>
